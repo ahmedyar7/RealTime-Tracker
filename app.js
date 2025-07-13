710a9c99eacac3de71ca0a3566c8cb78a7cf5cca
ahmedyar7 <ahmedyar778@gmail.com>
2025-07-13 13:54:28 +0500

    chore: set up HTTP server with Socket.io for real-time communication

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = socketio(server);
 
 app.get("/", (req, res) => {
   res.send("Server is Running");
