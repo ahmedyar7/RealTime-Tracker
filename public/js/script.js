1c433067e21bb5c1fe9c02fcffc8b21ff626ec78
ahmedyar7 <ahmedyar778@gmail.com>
2025-07-13 15:54:01 +0500

    Completing The mini Project

--- public/js/script.js
+++ public/js/script.js
@@ -20,5 +20,31 @@ if (navigator.geolocation) {
   });
 }
 
-Location.map("map");
-``;
+const map = L.map("map").setView([0, 0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Ahmed Yar",
+}).addTo(map);
+
+const markers = {};
+
+socket.on("received-location", (data) => {
+  const { id, latitude, longitude } = data;
+
+  map.setView([latitude, longitude], 20);
+
+  map.flyTo([latitude, longitude], 18, { animate: true, duration: 1 });
+
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
+
+socket.on("user-disconnected", (id) => {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
